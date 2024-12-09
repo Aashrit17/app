f7e73bc525b98ffac31f4bc08478c647215122de
Aashrit17 <aashrit17038gmail.comgit config --global user.name Aashrit17>
2024-12-09 10:35:14 +0545

    additionalcard details

--- lib/view/card_view.dart
+++ lib/view/card_view.dart
@@ -1,3 +1,5 @@
+import 'package:first_flutter_app/core/common/my_card.dart';
+import 'package:first_flutter_app/core/common/snackbar.dart';
 import 'package:flutter/material.dart';
 
 class CardView extends StatelessWidget {
@@ -11,37 +13,18 @@ class CardView extends StatelessWidget {
           child: Column(
             children: [
               for (int i = 1; i < 10; i++) ...{
-                MyCard(
-                  title: 'Card $i',
-                  isEven: i % 2 == 0,
+                GestureDetector(
+                  onTap: () {
+                    showMySnackBar(
+                        context: context, message: 'You pressed on Card $i');
+                  },
+                  child: MyCard(
+                    title: 'Card $i',
+                    color: i.isEven ? Colors.blue : Colors.amber,
+                  ),
                 )
               }
             ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class MyCard extends StatelessWidget {
-  const MyCard({super.key, required this.title, required this.isEven});
-
-  final String title;
-  final bool isEven;
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 200,
-      width: double.infinity,
-      child: Card(
-        elevation: 5,
-        color: isEven ? Colors.blue : Colors.amber,
-        child: Center(
-          child: Text(
-            title,
-            style: const TextStyle(fontSize: 30, color: Colors.white),
           ),
         ),
       ),
